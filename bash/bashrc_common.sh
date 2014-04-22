7c3ef7c8dfdcbc24d6f5cb91c1ba5946874e7a08
Nodir Turakulov <turakulov@gmail.com>
2014-04-21 18:15:09 -0700

    bash: PS1: git branch

--- bash/bashrc_common.sh
+++ bash/bashrc_common.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
 # Command prompt
-export PS1='\[\033[1;31m\]\W $ \[\033[0m\] '
+function command_prompt {
+  local __git_branch_color="\[\033[37m\]"
+  local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
+  export PS1="\[\033[1;31m\]\W $ $__git_branch_color$__git_branch\[\033[0m\] "
+}
+
+command_prompt
